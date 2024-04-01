fd8aa094423dbae6e76439ecc69176172508ce39
david_perso <davidboulanger2003@gmail.com>
2024-04-01 01:08:02 -0400

    ajout des files

---
+++ Simulateur.R
@@ -0,0 +1,34 @@
+sinistre <- read.csv("nath_perte")$x
+boost <- read.csv("nath_boosting")$x
+glm <- read.csv("nath_glm")$x
+
+primeChoisi <- function(v){
+  p1 <- min(v)
+  p2 <- max(v)
+  if(min(1, -0.504244*(exp(19.11*(p1 - p2)/p2)-1) +0.5) > runif(1)) {
+    return(p1)
+  }
+  p2
+}
+moy <- rep(mean(sinistre), 747628)
+primeweird <- c(boost[1:370000]*1.08, boost[370001:747628]*1.12)
+pi <- data.frame("boost" = moy*1.15,
+                 "b-g" = glm*1.20,
+                 "glm" = boost*1.20)
+
+
+risoto <- t(apply(pi, MARGIN = 1, FUN = function(x) {
+  idx <- sample(1:3, 2)
+  x == primeChoisi(x[idx])
+}))
+
+colMeans(risoto)
+
+sum(pi[risoto[,1], 1] - sinistre[risoto[,1]])/1000000
+sum(pi[risoto[,2], 2] - sinistre[risoto[,2]])/1000000
+sum(pi[risoto[,3], 3] - sinistre[risoto[,3]])/1000000
+
+mean(pi[risoto[,1], 1] - sinistre[risoto[,1]])
+mean(pi[risoto[,2], 2] - sinistre[risoto[,2]])
+mean(pi[risoto[,3], 3] - sinistre[risoto[,3]])
+
