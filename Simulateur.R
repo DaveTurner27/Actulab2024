1c7b05170f1f1ab59511930e57440ed52ddf48ae
Nathaniel Gaudreau <nagau162@ulaval.ca>
2024-04-02 01:36:43 -0400

    yo

--- Simulateur.R
+++ Simulateur.R
@@ -4,14 +4,13 @@ glm <- read.csv("nath_glm")$x
 
 primeChoisi <- function(v){
   p1 <- min(v)
-  p2 <- max(v)
+  p2 <- min(v[which(v =! p1)])
   if(min(1, -0.504244*(exp(19.11*(p1 - p2)/p2)-1) +0.5) > runif(1)) {
     return(p1)
   }
   p2
 }
 moy <- rep(mean(sinistre), 747628)
-primeweird <- c(boost[1:370000]*1.08, boost[370001:747628]*1.12)
 pi <- data.frame("boost" = moy*1.15,
                  "b-g" = glm*1.20,
                  "glm" = boost*1.20)
@@ -31,4 +30,3 @@ sum(pi[risoto[,3], 3] - sinistre[risoto[,3]])/1000000
 mean(pi[risoto[,1], 1] - sinistre[risoto[,1]])
 mean(pi[risoto[,2], 2] - sinistre[risoto[,2]])
 mean(pi[risoto[,3], 3] - sinistre[risoto[,3]])
-
